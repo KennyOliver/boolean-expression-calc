7b448eabc14961495b11fc37bd3a6b23f9a70823
Kenny <kenny_oliver@icloud.com>
2021-03-15 17:42:45 +0000

    [+] Created function bool_calc(bool_a,bool_b) and put code for calculating in there [+] Changed first_val & second_val to bool_a & bool_b, respectively (for within bool_calc()) [+] Slight tweak to UI to say “Enter 1-2 boolean values” instead of “1”

--- main.py
+++ main.py
@@ -1,28 +1,30 @@
 from VividHues import Clr
 #====================
+def bool_calc(bool_a,bool_b):
+  """ boolean calculator """
+  if bool_a in ["0","false"]:
+    bool_a = False
+  elif bool_a in ["1","true"]:
+    bool_a = True
+  
+  if bool_b in ["1","true"]:
+    bool_b = True
+  elif bool_b in ["0","false"]:
+    bool_b = False
+  
+  print(Clr.YELLOW + str(bool_a),"AND",str(bool_b) + Clr.RESET)
+  print("\t=",bool_a and bool_b)
+  
+  print(Clr.YELLOW + str(bool_a),"OR",str(bool_b) + Clr.RESET)
+  print("\t=",bool_a or bool_b)
+  
+  print(Clr.YELLOW + "NOT",str(bool_a),",","NOT",str(bool_b) + Clr.RESET)
+  print("\t=",not bool_a,",",not bool_b)
+#====================
 # MAIN PROGRAM
-expression = input(Clr.LIME + "Enter 2 boolean values (separate by a comma)\n\te.g. True,True\n\t--> " + Clr.RESET)
+expression = input(Clr.LIME + "Enter 1-2 boolean values (separate by a comma)\n\te.g. True,True\n\t--> " + Clr.RESET)
 
 values = expression.split(",")
 first_val, second_val = values[0].lower(), values[-1].lower()
 
-
-if first_val in ["0","false"]:
-  first_val = False
-elif first_val in ["1","true"]:
-  first_val = True
-
-if second_val in ["1","true"]:
-  second_val = True
-elif second_val in ["0","false"]:
-  second_val = False
-
-
-print(Clr.YELLOW + str(first_val),"AND",str(second_val) + Clr.RESET)
-print("\t=",first_val and second_val)
-
-print(Clr.YELLOW + str(first_val),"OR",str(second_val) + Clr.RESET)
-print("\t=",first_val or second_val)
-
-print(Clr.YELLOW + "NOT",str(first_val),",","NOT",str(second_val) + Clr.RESET)
-print("\t=",not first_val,",",not second_val)
+bool_calc(first_val,second_val)
